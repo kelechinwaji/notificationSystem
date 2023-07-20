abfed87ba75cc562fdf7b069e76a854b98202d78
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-07-20 07:25:25 +0100

    [feat]: handle user notification

--- src/app.js
+++ src/app.js
@@ -13,6 +13,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+// Endpoint to trigger the notification process
+app.post('/api/trigger-notification', async (req, res) => {
+    const userId = req.body.userId;
+    const amount = parseFloat(req.body.amount);
+    const notificationType = req.body.notificationType;
+  
+    try {
+      await handleFailedDepositNotification(userId, amount, notificationType);
+      res.status(200).json({ message: 'Notification process completed successfully.' });
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred during the notification process.' });
+    }
+  });
+
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
